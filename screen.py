880f499840dac5e39beb88fdbaec827532126fc9
karny4848 <karny4848@gmail.com>
2026-08-19 12:24:34 +0300

    pasta

--- screen.py
+++ screen.py
@@ -1,6 +1,8 @@
 import pygame
+import consts
 
-WINDOW=pygame.display.set_mode((500,500))
+WINDOW=pygame.display.set_mode((consts.WINDOW_WIDTH,consts.WINDOW_HEIGHT))
+WINDOW.fill((255,230,247))
 
 def display():
     pygame.display.flip()
