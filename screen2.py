c7aafcef59898d786e64f6a18f886535e4f89ec1
gali <galgalosha@gmail.com>
2026-08-19 15:35:06 +0300

    add

--- screen2.py
+++ screen2.py
@@ -2,16 +2,19 @@ import pygame
 import consts
 
 def draw_message_a(screen):
+    screen.fill(consts.BACKGROUND_COLOR)
     font = pygame.font.SysFont(consts.FONT_NAME, consts.FONT_SIZE)
     a_img = font.render(consts.MESSAGE_A, True, consts.COLOR)
     screen.blit(a_img, consts.FONT_LOCATION)
 
 def draw_message_b(screen):
+    screen.fill(consts.BACKGROUND_COLOR)
     font = pygame.font.SysFont(consts.FONT_NAME, consts.FONT_SIZE)
     b_img = font.render(consts.MESSAGE_B, True, consts.COLOR)
     screen.blit(b_img, consts.FONT_LOCATION)
 
 def draw_message_c(screen):
+    screen.fill(consts.BACKGROUND_COLOR)
     font = pygame.font.SysFont(consts.FONT_NAME, consts.FONT_SIZE)
     c_img = font.render(consts.MESSAGE_C, True, consts.COLOR)
     screen.blit(c_img, consts.FONT_LOCATION)
