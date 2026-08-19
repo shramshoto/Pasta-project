415c34969bed680b009261115a06aafbaa299fc3
Esther <pisaq11@gmail.com>
2026-08-19 23:59:04 +0300

    updated main and consts. main - added skeleton consts - made the location and start point separate consts to make dicts for main

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame,sys
 import Screen1
+import consts
 
-pygame.init()
+'''pygame.init()
 clock = pygame.time.Clock()
 while True:
     pygame.display.flip()
@@ -19,4 +20,32 @@ while True:
     '''Screen1.draw_message_a()
     Screen1.draw_message_b()
     Screen1.draw_message_c()'''
-    clock.tick(60)
+    clock.tick(60)'''
+
+# clock = pygame.time.Clock()
+running = True
+num = 0
+
+while running:
+    pygame.init()
+    pygame.display.flip()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mx, my = pygame.mouse.get_pos()
+            if mouse_clicking_obj(num, mx, my, consts.appearance):
+                #DO ANIMATION
+                pass
+            else:
+                pass
+
+pygame.quit()
+
+def mouse_clicking_obj(num, mx, my, list):
+    if list[num]["obj_x"]<mx<list[num]["obj_x"]+list[num]["width"] \
+        and list[num]["obj_y"]<my<list[num]["obj_y"]+list[num]["height"]:
+        num += 1
+        return True
+    return False
