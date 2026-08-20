7f121191dc57b3d2ca48d85e232da34d7d603767
gali <galgalosha@gmail.com>
2026-08-20 14:22:14 +0300

    add

--- Screen1.py
+++ Screen1.py
@@ -35,6 +35,7 @@ def draw_message_c():
     collage_img = pygame.transform.scale(consts.collage_img, consts.TOMATO_SIZE)
     screen.blit(collage_img, (330, 300))
 
+
 def draw_message_recipe():
     pygame.draw.rect(screen, consts.BACKGROUND_COLOR, pygame.Rect(900, 10, 100, 200))
     font = pygame.font.SysFont(consts.FONT_NAME, consts.FONT_SIZE_SMALL)
