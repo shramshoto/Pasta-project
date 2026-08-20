b721cacf00a4290d592f8a9dc207e36a112b87f1
Esther <pisaq11@gmail.com>
2026-08-20 11:01:16 +0300

    main

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ running = True
 num = 0
 max_num = len(consts.appearance)
 
+def mouse_clicking_obj(num, mx, my, list):
+    if list[num]["obj_x"]<mx<list[num]["obj_x"]+list[num]["width"] \
+        and list[num]["obj_y"]<my<list[num]["obj_y"]+list[num]["height"]:
+        return True
+    return False
+
+def do_animation(list, num):
+    for i in range(len(list[num]["animations"])):
+        list[num]["animations"][i]
+
 while running:
     pygame.init()
     pygame.display.flip()
@@ -18,7 +28,7 @@ while running:
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mx, my = pygame.mouse.get_pos()
                 if mouse_clicking_obj(num, mx, my, consts.appearance):
-                    #do_animation(appearance)
+                    #do_animation(consts.appearance)
                     num += 1
                 else:
                     pass
@@ -32,13 +42,3 @@ while running:
     clock.tick(60)
 
 pygame.quit()
-
-def mouse_clicking_obj(num, mx, my, list):
-    if list[num]["obj_x"]<mx<list[num]["obj_x"]+list[num]["width"] \
-        and list[num]["obj_y"]<my<list[num]["obj_y"]+list[num]["height"]:
-        return True
-    return False
-
-def do_animation(list, num):
-    for i in range(len(list[num]["animations"])):
-        list[num]["animations"][i]
