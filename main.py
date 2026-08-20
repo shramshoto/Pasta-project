0297bdea4182b7f1e3e8a9eab68e59220e6ed9da
Esther <pisaq11@gmail.com>
2026-08-20 13:13:46 +0300

    main

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import pygame
 import Screen1
 import spoon
+import consts
 
 clock = pygame.time.Clock()
 running = True
 num = 0
 max_num = len(spoon.SPOON)
 
-def mouse_clicking_obj(num, mx, my, list):
-    if list[num]["obj_x"]<mx<list[num]["obj_x"]+list[num]["width"] \
-        and list[num]["obj_y"]<my<list[num]["obj_y"]+list[num]["height"]:
+def mouse_clicking_obj(mx, my):
+    if spoon.SPOON["obj_x"]<mx<spoon.SPOON["obj_x"]+spoon.SPOON["width"] \
+        and spoon.SPOON["obj_y"]<my<spoon.SPOON["obj_y"]+spoon.SPOON["height"]:
         return True
     return False
 
-def do_animation(list, num):
-    for i in range(len(list[num]["animations"])):
-        if list[num]["state"]:
-            list[num]["animations"][i]
+def do_animation():
+    for i in range(len(spoon.SPOON["animations"])):
+        if consts.state:
+            spoon.SPOON["animations"][i]
 
 def write():
     if event.key == pygame.K_1:
@@ -28,6 +29,8 @@ def write():
 
 while running:
     pygame.init()
+    Screen1.draw_screen()
+    Screen1.draw_empty_spoon()
     pygame.display.flip()
 
     while num < max_num:
@@ -36,8 +39,8 @@ while running:
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mx, my = pygame.mouse.get_pos()
-                if mouse_clicking_obj(num, mx, my, spoon.SPOON):
-                    do_animation(spoon.SPOON, num)
+                if mouse_clicking_obj(mx, my):
+                    do_animation()
                     num += 1
                 else:
                     pass
