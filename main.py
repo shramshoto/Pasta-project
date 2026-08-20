186f91080b5c215516114a4ccc36798faf77fb30
Esther <pisaq11@gmail.com>
2026-08-20 13:56:12 +0300

    main, consts, Screen1

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ running = True
 num = 0
 max_num = len(spoon.SPOON)
 
+
+
 def mouse_clicking_obj(mx, my):
     if spoon.SPOON["obj_x"]<mx<spoon.SPOON["obj_x"]+spoon.SPOON["width"] \
         and spoon.SPOON["obj_y"]<my<spoon.SPOON["obj_y"]+spoon.SPOON["height"]:
@@ -15,35 +17,49 @@ def mouse_clicking_obj(mx, my):
     return False
 
 def do_animation():
-    for i in range(len(spoon.SPOON["animations"])):
-        if consts.state:
-            spoon.SPOON["animations"][i]
+    '''for i in range(len(spoon.SPOON["animations"])):
+        #if consts.state:
+        spoon.SPOON["animations"][i]'''
+    spoon.animate_scoop()
+    pygame.display.flip()
+    spoon.spoon_move()
+    pygame.display.flip()
+    spoon.animate_pour()
+    pygame.display.flip()
 
 def write():
     if event.key == pygame.K_1:
         Screen1.draw_message_a()
+        pygame.display.flip()
     elif event.key == pygame.K_2:
         Screen1.draw_message_b()
+        pygame.display.flip()
     elif event.key == pygame.K_3:
         Screen1.draw_message_c()
+        pygame.display.flip()
 
 while running:
+
     pygame.init()
     Screen1.drew_screen()
     Screen1.draw_empty_spoon()
     pygame.display.flip()
 
     while num < max_num:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                mx, my = pygame.mouse.get_pos()
-                if mouse_clicking_obj(mx, my):
-                    do_animation()
-                    num += 1
-                else:
-                    pass
+                consts.scooping = True
+
+                if consts.scooping:
+                    spoon.animate_pour()
+                elif consts.pouring:
+                    spoon.animate_scoop()
+                elif consts.move:
+                    spoon.spoon_move()
+
             elif event.type == pygame.KEYDOWN:
                 write()
 
--- scoop_animation.py
+++ scoop_animation.py
@@ -31,7 +31,8 @@ class scoop(pygame.sprite.Sprite):
 
         if self.current_sprite >= len(self.scoop_sprites):
             self.current_sprite = 0
-            consts.state = False
+            consts.scooping= False
+            consts.move = True
 
         self.image = self.scoop_sprites[int(self.current_sprite)]
 
