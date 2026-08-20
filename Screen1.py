286681265202afeb2553f751741fa2b8ee6b62de
gali <galgalosha@gmail.com>
2026-08-20 10:44:41 +0300

    add

--- Screen1.py
+++ Screen1.py
@@ -39,19 +39,19 @@ def draw_frame():
 
 
 def draw_message_a():
-    #screen.fill(consts.BACKGROUND_COLOR)
+    pygame.draw.rect(screen, consts.BACKGROUND_COLOR, pygame.Rect(50, 50, 900, 400))
     font = pygame.font.SysFont(consts.FONT_NAME, consts.FONT_SIZE)
     a_img = font.render(consts.MESSAGE_A, True, consts.COLOR)
     screen.blit(a_img, consts.FONT_LOCATION)
 
 def draw_message_b():
-    screen.fill(consts.BACKGROUND_COLOR)
+    pygame.draw.rect(screen, consts.BACKGROUND_COLOR, pygame.Rect(50, 50, 900, 400))
     font = pygame.font.SysFont(consts.FONT_NAME, consts.FONT_SIZE)
     b_img = font.render(consts.MESSAGE_B, True, consts.COLOR)
     screen.blit(b_img, consts.FONT_LOCATION)
 
 def draw_message_c():
-    screen.fill(consts.BACKGROUND_COLOR)
+    pygame.draw.rect(screen, consts.BACKGROUND_COLOR, pygame.Rect(50, 50, 900, 400))
     font = pygame.font.SysFont(consts.FONT_NAME, consts.FONT_SIZE)
     c_img = font.render(consts.MESSAGE_C, True, consts.COLOR)
     screen.blit(c_img, consts.FONT_LOCATION)
